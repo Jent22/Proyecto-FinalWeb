5ba6ab0e5b7f38c456ad30a8c7c9c77ccea594d2
Jent22 <72890247+Jent22@users.noreply.github.com>
2021-08-18 05:52:55 -0400

    5ta version

--- add-camiones.php
+++ add-camiones.php
@@ -58,63 +58,7 @@ require_once "addonlyphp.php";
                             <input name="Peso" class="form-control">
                             <span class="help-block"><?php echo $PesoError;?></span>
                         </div>
-                        
-                        <div class="wrapper">
-        <div class="container-fluid">
-            <div class="row">
-              <h2 class="text-center">Seleccione las lavadoras a agregar</h2>
-                <div class="col-md-12">
-                    <?php
-                     $rowSQL = mysqli_query( $conn,"SELECT MAX( ID ) AS max FROM `camiones`;" );
-                     if (mysqli_query($conn, $sql)) {
-                        $last_id = mysqli_insert_id($conn);
-                    }
-                    //  $row = mysqli_fetch_array( $rowSQL );
-                    //  $LastID = $row['max'];
-                    // $idcamion=($LastID+1);
-                    require_once "addonlyphp.php";
-                    $data = "SELECT * FROM lavadoras WHERE ID = $idcamion";
-                    if($users = mysqli_query($conn, $data)){
-                        if(mysqli_num_rows($users) > 0){
-                            echo "<table class='table table-bordered table-striped'>
-                                    <thead>
-                                      <tr>
-                                        <th>#</th>
-                                        <th>Marca</th>
-                                        <th>Modelo</th>
-                                        <th>Valor</th>
-                                        <th>Peso</th>
-                                      </tr>
-                                    </thead>
-                                    <tbody>";
-                                while($user = mysqli_fetch_array($users)) {
-                                    echo "<tr>
-                                            <td>" . $user['ID'] . "</td>
-                                            <td>" . $user['Marca'] . "</td>
-                                            <td>" . $user['Modelo'] . "</td>
-                                            <td>" . $user['Valor'] . "</td>
-                                            <td>" . $user['Peso'] . "</td>
-                                            <td>
-                                              <a href='edit.php?id=". $user['ID'] ."' title='Edit User' data-toggle='tooltip'><span class='glyphicon glyphicon-plus'></span></a>
-                                            </td>
-                                          </tr>";
-                                }
-                                echo "</tbody>
-                                </table>";
-                            mysqli_free_result($users);
-                        } else{
-                            echo "<p class='lead'><em>No hay registros.</em></p>";
-                        }
-                    } else{
-                        echo "ERROR: No se pudo ejecutar el $sql. " . mysqli_error($conn);
-                    }
-                    mysqli_close($conn);
-                    ?>
-                </div>
-            </div>
-        </div>
-    </div>
-                        <input type="submit" class="btn btn-primary" value="Aceptar">
+                        <input type="submit" class="btn btn-primary" value="Agregar registro">
                         <a href="index.php" class="btn btn-default">Cancelar</a>
                     </form>
                 </div>
